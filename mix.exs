bf5013fb89bf0cc6cfc40abd0bda88ca73d84787
Denver Abrey <denvera@gmail.com>
2019-06-03 13:36:54 +0200

    Add hex metadata

--- mix.exs
+++ mix.exs
@@ -9,7 +9,11 @@ defmodule LoggerAmqpBackend.MixProject do
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "logger_amqp_backend",
+      source_url: "https://github.com/denvera/logger_amqp_backend"
     ]
   end
 
@@ -22,8 +26,19 @@ defmodule LoggerAmqpBackend.MixProject do
   defp deps do
     [
       {:amqp, "~> 1.2"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:mock, "~> 0.3.0", only: :test},
       {:excoveralls, "~> 0.10", only: :test}
+    ]
+  end
+
+  defp description() do
+    "Elixir Logger backend to send logs to an AMQP broker (eg: RabbitMQ)"
+  end
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/denvera/logger_amqp_backen"}
     ]
   end
 end
